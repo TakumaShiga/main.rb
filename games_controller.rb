8a5340bf60cb66180268ed8662865c7c491233fe
TakumaShiga <tak07sweetvo@gmail.com>
2021-08-22 08:36:54 +0900

    games_controller.rb

---
+++ games_controller.rb
@@ -0,0 +1,26 @@
+class GamesController
+
+  EXP_CONSTANT = 2
+  GOLD_CONSTANT = 3
+
+  loop do
+    brave.attack(monster)
+    break if monster.hp <= 0
+
+    monster.attack(brave)
+    break if brave.hp <= 0
+  end
+
+  battle_result = brave.hp > 0
+
+  if battle_result
+    exp = (monster.offense + monster.defense) * EXP_CONSTANT
+    gold = (monster.offense + monster.defense) * GOLD_CONSTANT
+    puts "#{brave.name}はたたかいに勝った"
+    puts "#{exp}の経験値と#{gold}ゴールドを獲得した"
+  else
+    puts "#{brave.name}はたたかいに負けた"
+    puts "目の前が真っ暗になった"
+  end
+
+end
