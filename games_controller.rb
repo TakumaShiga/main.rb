414a51c73864c080aad18c84af26a6d3d961ac90
TakumaShiga <tak07sweetvo@gmail.com>
2021-08-22 08:55:46 +0900

    build_charactersメソッド

--- games_controller.rb
+++ games_controller.rb
@@ -8,35 +8,40 @@ class GamesController
     GOLD_CONSTANT = 3
 
     loop do
-      brave.attack(monster)
-      break if monster.hp <= 0
-      monster.attack(brave)
-      break if brave.hp <= 0
+      @brave.attack(@monster)
+      break if battle_end?
+      @monster.attack(@brave)
+      break if battle_end?
     end
 
-    if battle_result(brave)
-      result = calculate_of_exp_and_gold(monster)
-      puts "#{brave.name}はたたかいに勝った"
+    if battle_result
+      result = calculate_of_exp_and_gold
+      puts "#{@brave.name}はたたかいに勝った"
       puts "#{result[:exp]}の経験値と#{result[:gold]}ゴールドを獲得した"
     else
-      puts "#{brave.name}はたたかいに負けた"
+      puts "#{@brave.name}はたたかいに負けた"
       puts "目の前が真っ暗になった"
     end
   end
 
   private
 
-    def battle_end?(character)
+    def build_characters(**params)
+      @brave = params[:brave]
+      @monster = params[:monster]
+    end
+
+    def battle_end?
       character.hp < 0
     end
 
-    def battle_result?(brave)
-      brave.hp < 0
+    def battle_result
+      @brave.hp < 0
     end
 
-    def calculate_of_exp_and_gold(monster)
-      exp = (monster.offense + monster.defense) * EXP_CONSTANT
-      gold = (monster.offense + monster.defense) * GOLD_CONSTANT
+    def calculate_of_exp_and_gold
+      exp = (@monster.offense + @monster.defense) * EXP_CONSTANT
+      gold = (@monster.offense + @monster.defense) * GOLD_CONSTANT
       result = {exp: exp, gold: gold}
 
       result
