3e653849dfd68e109dfc6fa36d348aa93c8b9956
TakumaShiga <tak07sweetvo@gmail.com>
2021-08-21 10:28:53 +0900

    superメソッド

--- main.rb
+++ main.rb
@@ -70,10 +70,13 @@ class Monster < Character
   CALC_HALF_HP = 0.5
 
   def initialize(**params)
-    @name = params[:name]
-    @hp = params[:hp]
-    @offense = params[:offense]
-    @defense = params[:defense]
+    
+    super(
+      name: params[:name],
+      hp: params[:hp],
+      offense: params[:offense],
+      defense: params[:defense]
+    )
 
     @transform_flag = false
     @trigger_of_transform = params[:hp] * CALC_HALF_HP
